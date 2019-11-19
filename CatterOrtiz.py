782a04785a26ccd9b7a401ec44a711467f457ec6
llamo-unprg28 <57849468+llamo-unprg28@users.noreply.github.com>
2019-11-19 12:16:47 -0500

    se agrego programas de catter ortiz

---
+++ CatterOrtiz.py
@@ -0,0 +1,385 @@
+#PROGRAMA 1- CALCULAR EL TOTAL DEL IMPORTE
+#DECLARACION
+
+libro_de_auditoria_tributaria=0.0
+libro_manual_del_contador=0.0
+compendio_laboral=0.0
+total=0.0
+
+#INPUT
+libro_de_auditoria_tributaria=250.00
+libro_manual_del_contador=250.00
+compendio_laboral=300.00
+
+#PROCESSING
+total=(libro_de_auditoria_tributaria+libro_manual_del_contador+compendio_laboral)
+
+#OUTPUT
+print("#########################")
+print("#  TOTAL DEL IMPORTE   #")
+print("#########################")
+print("# libro de auditoria tributaria: ", libro_de_auditoria_tributaria)
+print("# libro manual del contador: ", libro_manual_del_contador)
+print("# compendio laboral: ", compendio_laboral)
+print("# total: ", total)
+print("#########################")
+
+
+#PROGRAMA 2- CALCULAR EL TOTAL DE LIBROS VENDIDOS
+#DECLARACION
+libro_de_auditoria_tributaria=0
+libro_manual_del_contador=0
+compendio_laboral=0
+total=0
+#INPUT
+libro_de_auditoria_tributaria=1
+libro_manual_del_contador=1
+compendio_laboral=1
+#PROCESSING
+total=(libro_de_auditoria_tributaria+libro_manual_del_contador+compendio_laboral)
+#OUTPUT
+print("#########################")
+print("#   TOTAL DE LIBROS VENDIDOS   #")
+print("#########################")
+print("# libro de auditoria tributaria: ", libro_de_auditoria_tributaria)
+print("# libro manual del contador: ", libro_manual_del_contador)
+print("# compendio laboral: ", compendio_laboral)
+print("# total: ", total)
+print("#########################")
+
+#PROGRAMA 3 - CALCULAR LA SUMA DE LOS QUE USAN CHROME Y FIREFOX EN AFRICA
+#DECLARAR
+chrome=0.0
+firefox=0.0
+total=0.0
+#INPUT
+chrome=51.17
+firefox=5.63
+#PROCESSING
+total=(chrome+firefox)
+#OUTPUT
+print("##########################################################")
+print("# SUMA DE LOS QUE USAN CHROME Y FIREFOX EN AFRICA  #")
+print("#####################################################################")
+print("# chrome: ", chrome)
+print("# firefox: ", firefox)
+print("# total: ", total)
+print("#####################################################################")
+
+
+#PROGRAMA 4 -CALCULAR LA SUMA DE LOS QUE USAN CHROME Y FIREFOX EN ASIA
+chrome=0.0
+firefox=0.0
+total=0.0
+#INPUT
+chrome=56.47
+firefox=3.25
+#PROCESSING
+total=(chrome+firefox)
+#OUTPUT
+print("##########################################################")
+print("# SUMA DE LOS QUE USAN CHROME Y FIREFOX EN ASIA  #")
+print("#####################################################################")
+print("# chrome: ", chrome)
+print("# firefox: ", firefox)
+print("# total: ", total)
+print("#####################################################################")
+
+
+#PROGRAMA 5 -CALCULAR LA SUMA DE LOS QUE USAN CHROME Y FIREFOX EN EUROPA
+chrome=0.0
+firefox=0.0
+total=0.0
+#INPUT
+chrome=57.03
+firefox=9.68
+#PROCESSING
+total=(chrome+firefox)
+#OUTPUT
+print("##########################################################")
+print("# SUMA DE LOS QUE USAN CHROME Y FIREFOX EN EUROPA  #")
+print("#####################################################################")
+print("# chrome: ", chrome)
+print("# firefox: ", firefox)
+print("# total: ", total)
+print("#####################################################################")
+
+
+#PROGRAMA 6 -CALCULAR LA SUMA DE LOS QUE USAN CHROME Y FIREFOX EN AMERICA DEL NORTE
+chrome=0.0
+firefox=0.0
+total=0.0
+#INPUT
+chrome=52.03
+firefox=5.62
+#PROCESSING
+total=(chrome+firefox)
+#OUTPUT
+print("##########################################################")
+print("# SUMA DE LOS QUE USAN CHROME Y FIREFOX EN AMERICA DEL NORTE  #")
+print("#####################################################################")
+print("# chrome: ", chrome)
+print("# firefox: ", firefox)
+print("# total: ", total)
+print("#####################################################################")
+
+
+#PROGRAMA 7 -CALCULAR LA SUMA DE LOS QUE USAN CHROME Y FIREFOX EN AMERICA DEL SUR
+chrome=0.0
+firefox=0.0
+total=0.0
+#INPUT
+chrome=81.56
+firefox=5.18
+#PROCESSING
+total=(chrome+firefox)
+#OUTPUT
+print("##########################################################")
+print("# SUMA DE LOS QUE USAN CHROME Y FIREFOX EN AMERICA DEL SUR  #")
+print("#####################################################################")
+print("# chrome: ", chrome)
+print("# firefox: ", firefox)
+print("# total: ", total)
+print("#####################################################################")
+
+
+#PROGRAMA 8 -CALCULAR LA SUMA DE LOS QUE USAN CHROME Y FIREFOX EN OCEANIA
+chrome=0.0
+firefox=0.0
+total=0.0
+#INPUT
+chrome=49.49
+firefox=5.14
+#PROCESSING
+total=(chrome+firefox)
+#OUTPUT
+print("##########################################################")
+print("# SUMA DE LOS QUE USAN CHROME Y FIREFOX EN OCEANIA  #")
+print("#####################################################################")
+print("# chrome: ", chrome)
+print("# firefox: ", firefox)
+print("# total: ", total)
+print("#####################################################################")
+
+
+#PROGRAMA 9 -CALCULAR LA SUMA DE LOS QUE USAN SAMSUNG INTERNET E INTERNET EXPLORE EN AFRICA
+samsung_internet=0.0
+internet_explore=0.0
+total=0.0
+#INPUT
+samsung_internet=3.27
+internet_explore=0.0
+#PROCESSING
+total=(samsung_internet+internet_explore)
+#OUTPUT
+print("############################################################################")
+print("# SUMA DE LOS QUE USAN SAMSUNG INTERNET E INTERNET EXPLORE EN AFRICA  #")
+print("#####################################################################")
+print("# samsung internet: ", samsung_internet)
+print("# internet explore: ", internet_explore)
+print("# total: ", total)
+print("#####################################################################")
+
+
+#PROGRAMA 10 -CALCULAR LA SUMA DE LOS QUE USAN SAMSUNG INTERNET E INTERNET EXPLORE EN ASIA
+samsung_internet=0.0
+internet_explore=0.0
+total=0.0
+#INPUT
+samsung_internet=3.16
+internet_explore=0.0
+#PROCESSING
+total=(samsung_internet+internet_explore)
+#OUTPUT
+print("############################################################################")
+print("# SUMA DE LOS QUE USAN SAMSUNG INTERNET E INTERNET EXPLORE EN ASIA  #")
+print("#####################################################################")
+print("# samsung internet: ", samsung_internet)
+print("# internet explore: ", internet_explore)
+print("# total: ", total)
+print("#####################################################################")
+
+#PROGRAMA 11 -CALCULAR LA SUMA DE LOS QUE USAN SAMSUNG INTERNET E INTERNET EXPLORE EN EUROPA
+samsung_internet=0.0
+internet_explore=0.0
+total=0.0
+#INPUT
+samsung_internet=2.83
+internet_explore=4.2
+#PROCESSING
+total=(samsung_internet+internet_explore)
+#OUTPUT
+print("############################################################################")
+print("# SUMA DE LOS QUE USAN SAMSUNG INTERNET E INTERNET EXPLORE EN EUROPA  #")
+print("#####################################################################")
+print("# samsung internet: ", samsung_internet)
+print("# internet explore: ", internet_explore)
+print("# total: ", total)
+print("#####################################################################")
+
+#PROGRAMA 12 -CALCULAR LA SUMA DE LOS QUE USAN SAMSUNG INTERNET E INTERNET EXPLORE EN AMERICA DEL NORTE
+samsung_internet=0.0
+internet_explore=0.0
+total=0.0
+#INPUT
+samsung_internet=1.91
+internet_explore=5.31
+#PROCESSING
+total=(samsung_internet+internet_explore)
+#OUTPUT
+print("############################################################################")
+print("# SUMA DE LOS QUE USAN SAMSUNG INTERNET E INTERNET EXPLORE EN AMERICA DEL NORTE #")
+print("#####################################################################")
+print("# samsung internet: ", samsung_internet)
+print("# internet explore: ", internet_explore)
+print("# total: ", total)
+print("#####################################################################")
+
+#PROGRAMA 13 -CALCULAR LA SUMA DE LOS QUE USAN SAMSUNG INTERNET E INTERNET EXPLORE EN AMERICA DEL SUR
+samsung_internet=0.0
+internet_explore=0.0
+total=0.0
+#INPUT
+samsung_internet=1.59
+internet_explore=1.29
+#PROCESSING
+total=(samsung_internet+internet_explore)
+#OUTPUT
+print("############################################################################")
+print("# SUMA DE LOS QUE USAN SAMSUNG INTERNET E INTERNET EXPLORE EN AMERICA DEL SUR #")
+print("#####################################################################")
+print("# samsung internet: ", samsung_internet)
+print("# internet explore: ", internet_explore)
+print("# total: ", total)
+print("#####################################################################")
+
+
+#PROGRAMA 14 -CALCULAR LA SUMA DE LOS QUE USAN SAMSUNG INTERNET E INTERNET EXPLORE EN OCEANIA
+samsung_internet=0.0
+internet_explore=0.0
+total=0.0
+#INPUT
+samsung_internet=2.3
+internet_explore=4
+#PROCESSING
+total=(samsung_internet+internet_explore)
+#OUTPUT
+print("############################################################################")
+print("# SUMA DE LOS QUE USAN SAMSUNG INTERNET E INTERNET EXPLORE EN OCEANIA#")
+print("#####################################################################")
+print("# samsung internet: ", samsung_internet)
+print("# internet explore: ", internet_explore)
+print("# total: ", total)
+print("#####################################################################")
+
+
+#PROGRAMA 15 -CALCULAR LA SUMA DE LOS QUE USAN SAMSUNG INTERNET Y CRHOME EN AFRICA
+samsung_internet=0.0
+chrome=0.0
+total=0.0
+#INPUT
+samsung_internet=3.27
+crhome=51.17
+#PROCESSING
+total=(samsung_internet+crhome)
+#OUTPUT
+print("############################################################################")
+print("# SUMA DE LOS QUE USAN SAMSUNG INTERNET Y CRHOME EN AFRICA #")
+print("#####################################################################")
+print("# samsung internet: ", samsung_internet)
+print("# crhome: ", crhome)
+print("# total: ", total)
+print("#####################################################################")
+
+
+#PROGRAMA 16 -CALCULAR LA SUMA DE LOS QUE USAN SAMSUNG INTERNET Y CRHOME EN ASIA
+samsung_internet=0.0
+chrome=0.0
+total=0.0
+#INPUT
+samsung_internet=3.16
+crhome=56.47
+#PROCESSING
+total=(samsung_internet+crhome)
+#OUTPUT
+print("############################################################################")
+print("# SUMA DE LOS QUE USAN SAMSUNG INTERNET Y CRHOME EN ASIA #")
+print("#####################################################################")
+print("# samsung internet: ", samsung_internet)
+print("# crhome: ", crhome)
+print("# total: ", total)
+print("#####################################################################")
+
+#PROGRAMA 17 -CALCULAR LA SUMA DE LOS QUE USAN SAMSUNG INTERNET Y CRHOME EN EUROPA
+samsung_internet=0.0
+chrome=0.0
+total=0.0
+#INPUT
+samsung_internet=2.83
+crhome=57.03
+#PROCESSING
+total=(samsung_internet+crhome)
+#OUTPUT
+print("############################################################################")
+print("# SUMA DE LOS QUE USAN SAMSUNG INTERNET Y CRHOME EN EUROPA #")
+print("#####################################################################")
+print("# samsung internet: ", samsung_internet)
+print("# crhome: ", crhome)
+print("# total: ", total)
+print("#####################################################################")
+
+
+#PROGRAMA 18 -CALCULAR LA SUMA DE LOS QUE USAN SAMSUNG INTERNET Y CRHOME EN AMERICA DEL NORTE
+samsung_internet=0.0
+chrome=0.0
+total=0.0
+#INPUT
+samsung_internet=1.91
+crhome=52.03
+#PROCESSING
+total=(samsung_internet+crhome)
+#OUTPUT
+print("############################################################################")
+print("# SUMA DE LOS QUE USAN SAMSUNG INTERNET Y CRHOME EN AMERICA DEL NORTE #")
+print("#####################################################################")
+print("# samsung internet: ", samsung_internet)
+print("# crhome: ", crhome)
+print("# total: ", total)
+print("#####################################################################")
+
+
+#PROGRAMA 19 -CALCULAR LA SUMA DE LOS QUE USAN SAMSUNG INTERNET Y CRHOME EN AMERICA DEL SUR
+samsung_internet=0.0
+chrome=0.0
+total=0.0
+#INPUT
+samsung_internet=1.59
+crhome=81.56
+#PROCESSING
+total=(samsung_internet+crhome)
+#OUTPUT
+print("############################################################################")
+print("# SUMA DE LOS QUE USAN SAMSUNG INTERNET Y CRHOME EN AMERICA DEL SUR #")
+print("#####################################################################")
+print("# samsung internet: ", samsung_internet)
+print("# crhome: ", crhome)
+print("# total: ", total)
+print("#####################################################################")
+
+#PROGRAMA 20 -CALCULAR LA SUMA DE LOS QUE USAN SAMSUNG INTERNET Y CRHOME EN OCEANIA
+samsung_internet=0.0
+chrome=0.0
+total=0.0
+#INPUT
+samsung_internet=2.3
+crhome=49.49
+#PROCESSING
+total=(samsung_internet+crhome)
+#OUTPUT
+print("############################################################################")
+print("# SUMA DE LOS QUE USAN SAMSUNG INTERNET Y CRHOME EN OCEANIA #")
+print("#####################################################################")
+print("# samsung internet: ", samsung_internet)
+print("# crhome: ", crhome)
+print("# total: ", total)
+print("#####################################################################")
