73b6acb59275936adfc85a2b4d58b90a12d5a165
llamo-unprg28 <57849468+llamo-unprg28@users.noreply.github.com>
2019-11-19 11:59:22 -0500

    se agrego programas de llamo tenorio

---
+++ LlamoTenorio.py
@@ -0,0 +1,411 @@
+#PROGRAMA 1 -CALCULAR LA SUMA DE LAS VENTAS NETAS DEL AÑO 2005 Y 2006
+ventas_netas_2005=0
+ventas_netas_2006=0
+total=0
+#INPUT
+ventas_netas_2005=15321
+ventas_netas_2006=18249
+#PROCESSING
+total=(ventas_netas_2005+ventas_netas_2006)
+#OUTPUT
+print("############################################################################")
+print("# SUMA DE LAS VENTAS NETAS DEL AÑO 2005 Y 2006 #")
+print("#####################################################################")
+print("# ventas netas 2005: ", ventas_netas_2005)
+print("# ventas netas 2006: ", ventas_netas_2006)
+print("# total: ", total)
+print("#####################################################################")
+
+
+#PROGRAMA 2 -CALCULAR LA SUMA DE LAS VENTAS NETAS DEL AÑO 2007 Y 2008
+ventas_netas_2007=0
+ventas_netas_2008=0
+total=0
+#INPUT
+ventas_netas_2007=20893
+ventas_netas_2008=20131
+#PROCESSING
+total=(ventas_netas_2007+ventas_netas_2008)
+#OUTPUT
+print("############################################################################")
+print("# SUMA DE LAS VENTAS NETAS DEL AÑO 2005 Y 2006 #")
+print("#####################################################################")
+print("# ventas netas 2007: ", ventas_netas_2007)
+print("# ventas netas 2008: ", ventas_netas_2008)
+print("# total: ", total)
+print("#####################################################################")
+
+
+#PROGRAMA 3 -CALCULAR EL INCREMENTO DE LA UTILIDAD BRUTA DEL AÑO 2005 Y 2006
+utilidad_bruta_2005=0
+utilidad_bruta_2006=0
+total=0
+#INPUT
+utilidad_bruta_2005=6050
+utilidad_bruta_2006=6600
+#PROCESSING
+total=(utilidad_bruta_2006-utilidad_bruta_2005)
+#OUTPUT
+print("############################################################################")
+print("# INCREMENTO DE LA UTILIDAD BRUTA DEL AÑO 2005 Y 2006 #")
+print("#####################################################################")
+print("# utilidad bruta 2005: ", utilidad_bruta_2005)
+print("# utilidad bruta 2006: ", utilidad_bruta_2006)
+print("# total: ", total)
+print("#####################################################################")
+
+
+#PROGRAMA 4 -CALCULAR EL INCREMENTO DE LA UTILIDAD BRUTA DEL AÑO 2006 Y 2007
+utilidad_bruta_2006=0
+utilidad_bruta_2007=0
+total=0
+#INPUT
+utilidad_bruta_2006=6600
+utilidad_bruta_2007=7025
+#PROCESSING
+total=(utilidad_bruta_2007-utilidad_bruta_2006)
+#OUTPUT
+print("############################################################################")
+print("# INCREMENTO DE LA UTILIDAD BRUTA DEL AÑO 2006 Y 2007 #")
+print("#####################################################################")
+print("# utilidad bruta 2006: ", utilidad_bruta_2006)
+print("# utilidad bruta 2007: ", utilidad_bruta_2007)
+print("# total: ", total)
+print("#####################################################################")
+
+
+#PROGRAMA 5 -CALCULAR LA SUMA DE LOS COSTOS DE VENTA DEL AÑO 2005 Y 2006
+costos_de_ventas_2005=0
+costos_de_ventas_2006=0
+total=0
+#INPUT
+costos_de_ventas_2005=9271
+costos_de_ventas_2006=11649
+#PROCESSING
+total=(costos_de_ventas_2005+costos_de_ventas_2006)
+#OUTPUT
+print("############################################################################")
+print("# SUMA DE LOS COSTOS DE VENTA DEL AÑO 2005 Y 2006 #")
+print("#####################################################################")
+print("# costos de ventas 2005: ", costos_de_ventas_2005)
+print("# costos de ventas 2006: ", costos_de_ventas_2006)
+print("# total: ", total)
+print("#####################################################################")
+
+#PROGRAMA 6 -CALCULAR LA SUMA DE LOS COSTOS DE VENTA DEL AÑO 2006 Y 2007
+costos_de_ventas_2006=0
+costos_de_ventas_2007=0
+total=0
+#INPUT
+costos_de_ventas_2006=11649
+costos_de_ventas_2007=13868
+#PROCESSING
+total=(costos_de_ventas_2005+costos_de_ventas_2006)
+#OUTPUT
+print("############################################################################")
+print("# SUMA DE LOS COSTOS DE VENTA DEL AÑO 2006 Y 2007 #")
+print("#####################################################################")
+print("# costos de ventas 2006: ", costos_de_ventas_2006)
+print("# costos de ventas 2007: ", costos_de_ventas_2007)
+print("# total: ", total)
+print("#####################################################################")
+
+
+#PROGRAMA 7 -CALCULAR LA SUMA DE LOS COSTOS DE VENTA DEL AÑO 2007 HASTA 2009
+costos_de_ventas_2007=0
+costos_de_ventas_2008=0
+costos_de_ventas_2009=0
+total=0
+#INPUT
+costos_de_ventas_2007=13868
+costos_de_ventas_2008=13735
+costos_de_ventas_2009=10270
+#PROCESSING
+total=(costos_de_ventas_2007+costos_de_ventas_2008+costos_de_ventas_2009)
+#OUTPUT
+print("############################################################################")
+print("# SUMA DE LOS COSTOS DE VENTA DEL AÑO 2007 HASTA 2009 #")
+print("#####################################################################")
+print("# costos de ventas 2007: ", costos_de_ventas_2007)
+print("# costos de ventas 2008: ", costos_de_ventas_2008)
+print("# costos de ventas 2009: ", costos_de_ventas_2009)
+print("# total: ", total)
+print("#####################################################################")
+
+
+#PROGRAMA 8 -CALCULAR EL PRODUCTO DEL TOTAL ACTIVO CON EL TOTAL PASIVO AÑO 2005
+total_activo_2005=0
+total_pasivo_2005=0
+total=0
+#INPUT
+total_activo_2005=26763
+total_pasivo_2005=16409
+#PROCESSING
+total=(total_activo_2005*total_pasivo_2005)
+#OUTPUT
+print("############################################################################")
+print("# PRODUCTO DEL TOTAL ACTIVO CON EL TOTAL PASIVO AÑO 2005 #")
+print("#####################################################################")
+print("# total activo 2005: ", total_activo_2005)
+print("# total pasivo 2005: ", total_pasivo_2005)
+print("# total: ", total)
+print("#####################################################################")
+
+
+#PROGRAMA 9 -CALCULAR EL PRODUCTO DEL TOTAL ACTIVO CON EL TOTAL PASIVO AÑO 2006
+total_activo_2006=0
+total_pasivo_2006=0
+total=0
+#INPUT
+total_activo_2006=29972
+total_pasivo_2006=15193
+#PROCESSING
+total=(total_activo_2006*total_pasivo_2006)
+#OUTPUT
+print("############################################################################")
+print("# PRODUCTO DEL TOTAL ACTIVO CON EL TOTAL PASIVO AÑO 2006 #")
+print("#####################################################################")
+print("# total activo 2006: ", total_activo_2006)
+print("# total pasivo 2006: ", total_pasivo_2006)
+print("# total: ", total)
+print("#####################################################################")
+
+
+#PROGRAMA 10 -CALCULAR EL PRODUCTO DEL TOTAL ACTIVO CON EL TOTAL PASIVO AÑO 2007
+total_activo_2007=0
+total_pasivo_2007=0
+total=0
+#INPUT
+total_activo_2007=49662
+total_pasivo_2007=30967
+#PROCESSING
+total=(total_activo_2007*total_pasivo_2007)
+#OUTPUT
+print("############################################################################")
+print("# PRODUCTO DEL TOTAL ACTIVO CON EL TOTAL PASIVO AÑO 2007 #")
+print("#####################################################################")
+print("# total activo 2007: ", total_activo_2007)
+print("# total pasivo 2007: ", total_pasivo_2007)
+print("# total: ", total)
+print("#####################################################################")
+
+
+#PROGRAMA 11 -CALCULAR EL PRODUCTO DEL TOTAL ACTIVO CON EL TOTAL PASIVO AÑO 2008
+total_activo_2008=0
+total_pasivo_2008=0
+total=0
+#INPUT
+total_activo_2008=45387
+total_pasivo_2008=28119
+#PROCESSING
+total=(total_activo_2008*total_pasivo_2008)
+#OUTPUT
+print("############################################################################")
+print("# PRODUCTO DEL TOTAL ACTIVO CON EL TOTAL PASIVO AÑO 2008 #")
+print("#####################################################################")
+print("# total activo 2008: ", total_activo_2008)
+print("# total pasivo 2008: ", total_pasivo_2008)
+print("# total: ", total)
+print("#####################################################################")
+
+
+#PROGRAMA 12 -CALCULAR EL PRODUCTO DEL TOTAL ACTIVO CON EL TOTAL PASIVO AÑO 2009
+total_activo_2009=0
+total_pasivo_2009=0
+total=0
+#INPUT
+total_activo_2009=44483
+total_pasivo_2009=24806
+#PROCESSING
+total=(total_activo_2009*total_pasivo_2009)
+#OUTPUT
+print("############################################################################")
+print("# PRODUCTO DEL TOTAL ACTIVO CON EL TOTAL PASIVO AÑO 2009 #")
+print("#####################################################################")
+print("# total activo 2009: ", total_activo_2009)
+print("# total pasivo 2009: ", total_pasivo_2009)
+print("# total: ", total)
+print("#####################################################################")
+
+
+#PROGRAMA 13 -CALCULAR LA SUMA DE LOS QUE USAN EDGE, SAFARI Y OPERA EN AFRICA
+edge=0.0
+safari=0.0
+opera=0.0
+#INPUT
+edge=0
+safari=2.25
+opera=20.65
+
+#PROCESSING
+total=(edge+safari+opera)
+#OUTPUT
+print("############################################################################")
+print("# SUMA DE LOS QUE USAN EDGE, SAFARI Y OPERA EN AFRICA #")
+print("#####################################################################")
+print("# edge: ", edge)
+print("# safari: ", safari)
+print("# opera: ", opera)
+print("# total: ", total)
+print("#####################################################################")
+
+
+#PROGRAMA 14 -CALCULAR LA SUMA DE LOS QUE USAN EDGE, SAFARI Y OPERA EN ASIA
+edge=0.0
+safari=0.0
+opera=0.0
+#INPUT
+edge=0
+safari=7.74
+opera=4.66
+
+#PROCESSING
+total=(edge+safari+opera)
+#OUTPUT
+print("############################################################################")
+print("# SUMA DE LOS QUE USAN EDGE, SAFARI Y OPERA EN ASIA #")
+print("#####################################################################")
+print("# edge: ", edge)
+print("# safari: ", safari)
+print("# opera: ", opera)
+print("# total: ", total)
+print("#####################################################################")
+
+
+#PROGRAMA 15 -CALCULAR LA SUMA DE LOS QUE USAN EDGE, SAFARI Y OPERA EN EUROPA
+edge=0.0
+safari=0.0
+opera=0.0
+#INPUT
+edge=3.2
+safari=16.79
+opera=0
+
+#PROCESSING
+total=(edge+safari+opera)
+#OUTPUT
+print("############################################################################")
+print("# SUMA DE LOS QUE USAN EDGE, SAFARI Y OPERA EN EUROPA #")
+print("#####################################################################")
+print("# edge: ", edge)
+print("# safari: ", safari)
+print("# opera: ", opera)
+print("# total: ", total)
+print("#####################################################################")
+
+
+
+#PROGRAMA 16 -CALCULAR LA SUMA DE LOS QUE USAN EDGE, SAFARI Y OPERA EN AMERICA DEL NORTE
+edge=0.0
+safari=0.0
+opera=0.0
+#INPUT
+edge=3.7
+safari=28.09
+opera=0
+
+#PROCESSING
+total=(edge+safari+opera)
+#OUTPUT
+print("############################################################################")
+print("# SUMA DE LOS QUE USAN EDGE, SAFARI Y OPERA EN AMERICA DEL NORTE #")
+print("#####################################################################")
+print("# edge: ", edge)
+print("# safari: ", safari)
+print("# opera: ", opera)
+print("# total: ", total)
+print("#####################################################################")
+
+
+#PROGRAMA 17 -CALCULAR LA SUMA DE LOS QUE USAN EDGE, SAFARI Y OPERA EN AMERICA DEL SUR
+edge=0.0
+safari=0.0
+opera=0.0
+#INPUT
+edge=0
+safari=4.96
+opera=1.76
+
+#PROCESSING
+total=(edge+safari+opera)
+#OUTPUT
+print("############################################################################")
+print("# SUMA DE LOS QUE USAN EDGE, SAFARI Y OPERA EN AMERICA DEL SUR#")
+print("#####################################################################")
+print("# edge: ", edge)
+print("# safari: ", safari)
+print("# opera: ", opera)
+print("# total: ", total)
+print("#####################################################################")
+
+
+
+#PROGRAMA 18 -CALCULAR LA SUMA DE LOS QUE USAN EDGE, SAFARI Y OPERA EN OCEANIA
+edge=0.0
+safari=0.0
+opera=0.0
+#INPUT
+edge=3.47
+safari=31.4
+opera=0
+
+#PROCESSING
+total=(edge+safari+opera)
+#OUTPUT
+print("############################################################################")
+print("# SUMA DE LOS QUE USAN EDGE, SAFARI Y OPERA EN OCEANIA #")
+print("#####################################################################")
+print("# edge: ", edge)
+print("# safari: ", safari)
+print("# opera: ", opera)
+print("# total: ", total)
+print("#####################################################################")
+
+
+#PROGRAMA 1- CALCULAR LA SUMA DE LOS DOS PRIMEROS PRODUCTOS VENDIDOS
+#DECLARACION
+
+libro_de_auditoria_tributaria=0.0
+libro_manual_del_contador=0.0
+total=0.0
+
+#INPUT
+libro_de_auditoria_tributaria=250.00
+libro_manual_del_contador=250.00
+
+#PROCESSING
+total=(libro_de_auditoria_tributaria+libro_manual_del_contador)
+
+#OUTPUT
+print("#####################################################")
+print("#  SUMA DE LOS DOS PRIMEROS PRODUCTOS VENDIDOS  #")
+print("#########################")
+print("# libro de auditoria tributaria: ", libro_de_auditoria_tributaria)
+print("# libro manual del contador: ", libro_manual_del_contador)
+print("# total: ", total)
+print("#########################")
+
+
+#PROGRAMA 1- CALCULAR LA DIVISION DEL PRIMER PRODUCTO Y EL TOTAL DEL IMPORTE
+#DECLARACION
+
+libro_de_auditoria_tributaria=0.0
+total_del_importe=0.0
+total=0.0
+
+#INPUT
+libro_de_auditoria_tributaria=250
+total_del_importe=800
+
+#PROCESSING
+total=(libro_de_auditoria_tributaria/total_del_importe)
+
+#OUTPUT
+print("###########################################################")
+print("#  DIVISION DEL PRIMER PRODUCTO Y EL TOTAL DEL IMPORTE  #")
+print("#########################")
+print("# libro de auditoria tributaria: ", libro_de_auditoria_tributaria)
+print("# total del importe: ", total_del_importe)
+print("# total: ", total)
+print("#########################################################")
+
